6513eb70e1951970d23aae8366f87d7c436fdcbe
Nidarshan Siddegowda <nidarshans@gmail.com>
2021-02-09 10:49:04 -0800

    changes

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add *
 git commit -m "changes"
-ssh-agent sh -c 'ssh-add ../../../.ssh/git; git push'
+ssh-agent sh -c 'ssh -o UserKnownHostsFile="../../.ssh/known_hosts"; ssh-add ../../../.ssh/git; git push'
